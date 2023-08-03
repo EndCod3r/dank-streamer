c68e9d4337d1374ae63dc4df6467895203275edd
Endlord15 <endlord15@proton.me>
2023-08-02 19:41:00 -0800

    Changed index.py

--- index.py
+++ index.py
@@ -5,15 +5,31 @@ import random
 # Sets mouse as a variable to Controller()
 mouse = Controller()
 
-RunADPos = 2350
-ReadChatPos = 2440
-CollectDonationsPos = 2570
+# Gets the positions for each button
+print("Hover over Run AD Button in less than 5 seconds.")
+time.sleep(5)
+RunADPos = mouse.position
+print("Hover over Read Chat Button in less than 5 seconds.")
+time.sleep(5)
+ReadChatPos = mouse.position
+print("Hover over Collect Donations Button in less than 5 seconds.")
+time.sleep(5)
+CollectDonationsPos = mouse.position
+print(
+    "Hover over the Dismiss Message Button in Discord by clicking one of the buttons in less than 5 seconds."
+)
+time.sleep(5)
+DismissMessagePos = mouse.position
+
+print("Open Discord in less than 5 seconds and start streaming.")
+time.sleep(5)
+
 
 # Makes a infinite loop for auto streaming.
 while True:
     # Moves mouse to Dismiss messsage button and clicks it
     time.sleep(5)
-    mouse.position = (2475, 1025)
+    mouse.position = DismissMessagePos
     time.sleep(0.5)
     mouse.click(Button.left, 1)
 
@@ -21,14 +37,14 @@ while True:
     time.sleep(600)
     button = random.randint(1, 3)
     if button == 1:
-        mouse.position = (RunADPos, 1000)
+        mouse.position = RunADPos
         time.sleep(0.3)
         mouse.click(Button.left, 1)
     elif button == 2:
-        mouse.position = (ReadChatPos, 1000)
+        mouse.position = ReadChatPos
         time.sleep(0.3)
         mouse.click(Button.left, 1)
     elif button == 3:
-        mouse.position = (CollectDonationsPos, 1000)
+        mouse.position = CollectDonationsPos
         time.sleep(0.3)
         mouse.click(Button.left, 1)
